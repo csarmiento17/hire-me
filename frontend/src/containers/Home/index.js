37d5d35dee1e3afc44b38f2083de67070a525de5
Mikhael Hamoy+656+654 <mikhaelhamoy@gmail.com>
2021-11-12 03:33:18 -0500

    clean up homepage code

--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,24 +1,12 @@
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputAdornment from '@mui/material/InputAdornment';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-
-import SearchIcon from '@mui/icons-material/Search';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 import React, { useEffect, useState } from "react";
 
 
 const Home = () => {
-
-  const [values, setValues] = React.useState({
-    searchParameter: '',
-    location: ''
-  });
 
   return (
     <>
