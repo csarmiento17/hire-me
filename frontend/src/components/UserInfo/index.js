2f431a5978e5dc45c8e9995a953b32e5fbe93278
Siyan Guo <siyang@Hey-There.local>
2021-11-22 22:45:10 -0500

    update Profile Page to automatically render the userData

--- frontend/src/components/UserInfo/index.js
+++ frontend/src/components/UserInfo/index.js
@@ -18,16 +18,9 @@ export default function UserInfo() {
     data?.me?.savedJobs && setUserData(data?.me);
   }, [data, loading]);
 
-  useEffect(() => {
-    async function refetchData() {
-      await refetch();
-    }
-    return () => {
-      refetchData();
-    };
-  }, [refetch]);
-
-
+  useEffect(async () => {
+    const result = await refetch();
+  }, [])
 
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -39,16 +32,16 @@ export default function UserInfo() {
                 <CardContent>           
                 <Divider/>                    
                   <Typography variant="subtitle1" component="div" sx={{margin :1}}>
-                    Full name: {userData?.lastName},{userData?.firstName}
+                    My Name: {userData?.firstName} {userData?.lastName}
                   </Typography>
                   <Divider/>               
                   <Typography variant="subtitle1" component="div" sx={{margin :1}}>
-                    Email: {userData?.email}
+                    My Email: {userData?.email}
                   </Typography>     
                   <Divider/>             
                   <Typography variant="subtitle1" component="div"  sx={{margin :1}}>
                     Number of Saved Jobs:  <Chip 
-                    label={userData?.savedJobsCount ? userData?.savedJobsCount: 'No saved job'} variant="outlined">
+                    label={userData?.savedJobsCount ? userData?.savedJobsCount: 'No saved job'} variant="outlined" sx={{marginLeft:2}}>
                     </Chip>
                   </Typography>
                   <Divider/>
@@ -60,7 +53,7 @@ export default function UserInfo() {
                   </Typography> 
                   <Divider/>
                   <Typography variant="subtitle1" component="div"  sx={{margin :1}}>
-                    Length of Subscription
+                    Length of Subscription:
                       <Chip 
                         label={userData?.lengthtOfSubscription ?  userData?.lengthtOfSubscription : 'No subscription'} variant="outlined"  sx={{marginLeft:2}}>
                       </Chip>
