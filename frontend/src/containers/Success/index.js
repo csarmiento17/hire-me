9cd11639933b0ddf6c0c88533b008fe3348dc3cb
Mikhael Hamoy+656+654 <mikhaelhamoy@gmail.com>
2021-11-23 19:28:07 -0500

    update success background

--- frontend/src/containers/Success/index.js
+++ frontend/src/containers/Success/index.js
@@ -38,7 +38,7 @@ function Success() {
         spacing={2}
         sx={{ justifyContent: "center", marginTop: "1em" }}
       >
-        <Paper elevation={2} sx={{ p: 5, background: "primary", width: "50%" }}>
+        <Paper elevation={2} sx={{ p: 5, background: "success.main", width: "50%" }}>
           <Box sx={{ display: "flex", flexDirection: "column", color: "white"}}>
             <div className="successContent"><CheckCircleOutlineIcon sx={{ fontSize: 65 }} /></div>
             <div className="successContent"><h1 className="successHeader">Success!</h1></div>
